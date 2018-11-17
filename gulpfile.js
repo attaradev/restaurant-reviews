c4dedb328c5f94a852d49d1f6ddc2f45181d30b6
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2018-11-17 13:25:53 +0000

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,30 @@ const sass = require('gulp-sass');
 const reload = browserSync.reload;
 
 const src = {
-    scss: 'app/scss/*.scss',
-    css:  'app/css',
-    html: 'app/*.html'
+  sass: 'sass/**/*.scss',
+  css: 'app/css',
+  html: 'app/*.html'
 };
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], function () {
 
-    browserSync.init({
-        server: "./app"
-    });
+  browserSync.init({
+    server: "./app"
+  });
 
-    gulp.watch(src.scss, ['sass']);
-    gulp.watch(src.html).on('change', reload);
+  gulp.watch(src.sass, ['sass']);
+  gulp.watch(src.html).on('change', reload);
 });
 
 // Compile sass into CSS
-gulp.task('sass', function() {
-    return gulp.src(src.scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(src.css))
-        .pipe(reload({stream: true}));
+gulp.task('sass', function () {
+  return gulp.src(src.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(src.css))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 gulp.task('default', ['serve']);
